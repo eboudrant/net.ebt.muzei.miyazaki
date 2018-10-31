aa6a7a0a8cf0153b696ae0510227086b30b90bff
Ian Lake <ian.hannibal.lake@gmail.com>
2018-10-30 20:54:07 -0700

    Implement openArtworkInfo()
    Use Intent.ACTION_WEB_SEARCH to open information about the movie an artwork is associated with.

--- app/src/main/java/net/ebt/muzei/miyazaki/GhibliArtProvider.kt
+++ app/src/main/java/net/ebt/muzei/miyazaki/GhibliArtProvider.kt
@@ -1,10 +1,31 @@
 package net.ebt.muzei.miyazaki
 
+import android.app.SearchManager
+import android.content.Intent
+import com.google.android.apps.muzei.api.provider.Artwork
 import com.google.android.apps.muzei.api.provider.MuzeiArtProvider
 import net.ebt.muzei.miyazaki.load.ArtworkLoadWorker
 
 class GhibliArtProvider : MuzeiArtProvider() {
     override fun onLoadRequested(initial: Boolean) {
         ArtworkLoadWorker.enqueueLoad()
+    }
+
+    override fun openArtworkInfo(artwork: Artwork): Boolean {
+        val context = context ?: return false
+        val byline = artwork.byline
+        if (byline != null && byline.contains('-')) {
+            try {
+                val query = byline.substring(0, byline.indexOf('-'))
+                val intent = Intent(Intent.ACTION_WEB_SEARCH).apply {
+                    putExtra(SearchManager.QUERY, query)
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }
+                context.startActivity(intent)
+                return true
+            } catch (e: Exception) {
+            }
+        }
+        return false
     }
 }
