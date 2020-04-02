4164928ad4267dea7c504a2cbbc810be8b6416e3
Ian Lake <ian.hannibal.lake@gmail.com>
2020-04-01 22:10:56 -0700

    Upgrade Muzei API 3.2.0
    Switch from `openArtworkInfo()` to `getArtworkInfo()` to fix opening artwork info on API 29+ devices.

--- android-common/src/main/java/net/ebt/muzei/miyazaki/GhibliArtProvider.kt
+++ android-common/src/main/java/net/ebt/muzei/miyazaki/GhibliArtProvider.kt
@@ -1,5 +1,6 @@
 package net.ebt.muzei.miyazaki
 
+import android.app.PendingIntent
 import android.app.SearchManager
 import android.content.Intent
 import com.google.android.apps.muzei.api.provider.Artwork
@@ -21,21 +22,18 @@ class GhibliArtProvider : MuzeiArtProvider() {
         }
     }
 
-    override fun openArtworkInfo(artwork: Artwork): Boolean {
-        val context = context ?: return false
+    override fun getArtworkInfo(artwork: Artwork): PendingIntent? {
+        val context = context ?: return null
         val byline = artwork.byline
         if (byline != null && byline.contains('-')) {
-            try {
-                val query = byline.substring(0, byline.indexOf('-'))
-                val intent = Intent(Intent.ACTION_WEB_SEARCH).apply {
-                    putExtra(SearchManager.QUERY, query)
-                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                }
-                context.startActivity(intent)
-                return true
-            } catch (e: Exception) {
+            val query = byline.substring(0, byline.indexOf('-'))
+            val intent = Intent(Intent.ACTION_WEB_SEARCH).apply {
+                putExtra(SearchManager.QUERY, query)
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             }
+            return PendingIntent.getActivity(context, artwork.id.toInt(), intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT)
         }
-        return false
+        return null
     }
 }
