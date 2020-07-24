e8f9982055efde151823ade751f085aa20b4920e
Ian Lake <ian.hannibal.lake@gmail.com>
2020-07-23 19:24:40 -0700

    Upgrade to Muzei API 3.4
    Muzei API 3.4 comes with a number of important features:
    
    - Improved performance when adding many images at the same time (Ghibli now takes <5 seconds to load all artwork after being selected for the first time)
    - A `MuzeiArtDocumentsProvider` that lets us show Studio Ghibli art in the default Files picker
    - New `MuzeiContract` APIs for deep linking to the Muzei Sources screen to activate Studio Ghibli. This is only used in the `RedirectActivity` for API 29+ devices where having a launcher icon is required.
    
    This updates us to `compileSdkVersion` (and `targetSdkVersion`) to API 30 as is required by Muzei API 3.4.0.

---
+++ app/src/main/java/net/ebt/muzei/miyazaki/RedirectActivity.kt
@@ -0,0 +1,77 @@
+package net.ebt.muzei.miyazaki
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.StringRes
+import com.google.android.apps.muzei.api.MuzeiContract.Sources.createChooseProviderIntent
+import com.google.android.apps.muzei.api.MuzeiContract.Sources.isProviderSelected
+import net.ebt.muzei.miyazaki.common.BuildConfig
+
+/**
+ * This activity's sole purpose is to redirect users to Muzei, which is where they should
+ * activate Muzei and then select the Earth View source.
+ *
+ * You'll note the usage of the `enable_launcher` boolean resource value to only enable
+ * this on API 29+ devices as it is on API 29+ that a launcher icon becomes mandatory for
+ * every app.
+ */
+class RedirectActivity : Activity() {
+
+    companion object {
+        private const val MUZEI_PACKAGE_NAME = "net.nurik.roman.muzei"
+        private const val PLAY_STORE_LINK = "https://play.google.com/store/apps/details?id=$MUZEI_PACKAGE_NAME"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // First check whether Ghibli is already selected
+        val launchIntent = packageManager.getLaunchIntentForPackage(MUZEI_PACKAGE_NAME)
+        if (isProviderSelected(this, BuildConfig.GHIBLI_AUTHORITY)
+                && launchIntent != null) {
+            // Already selected so just open Muzei
+            startActivityForResult(launchIntent, 1)
+            return
+        }
+        // Ghibli isn't selected, so try to deep link into Muzei's Sources screen
+        val deepLinkIntent = createChooseProviderIntent(BuildConfig.GHIBLI_AUTHORITY)
+        if (tryStartIntent(deepLinkIntent, R.string.toast_enable)) {
+            return
+        }
+        // createChooseProviderIntent didn't work, so try to just launch Muzei
+        if (launchIntent != null && tryStartIntent(launchIntent, R.string.toast_enable_source)) {
+            return
+        }
+        // Muzei isn't installed, so try to open the Play Store so that
+        // users can install Muzei
+        val playStoreIntent = Intent(Intent.ACTION_VIEW).setData(Uri.parse(PLAY_STORE_LINK))
+        if (tryStartIntent(playStoreIntent, R.string.toast_muzei_missing_error)) {
+            return
+        }
+        // Only if all Intents failed do we show a 'everything failed' Toast
+        Toast.makeText(this, R.string.toast_play_store_missing_error, Toast.LENGTH_LONG).show()
+        finish()
+    }
+
+    private fun tryStartIntent(intent: Intent, @StringRes toastResId: Int): Boolean {
+        return try {
+            // Use startActivityForResult() so that we get a callback to
+            // onActivityResult() if the user hits the system back button
+            startActivityForResult(intent, 1)
+            Toast.makeText(this, toastResId, Toast.LENGTH_LONG).show()
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        // It doesn't matter what the result is, the important part is that the
+        // user hit the back button to return to this activity. Since this activity
+        // has no UI of its own, we can simply finish the activity.
+        super.onActivityResult(requestCode, resultCode, data)
+        finish()
+    }
+}
